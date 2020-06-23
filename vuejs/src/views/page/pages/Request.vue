b9ca3c98d18b65cf96acc3eac352b496c36989b6
Shan.tk <tksudharshan@gmail.com>
2020-06-23 21:05:08 +0530

    Complete Revamp of Request and Register Page

--- vuejs/src/views/page/pages/Request.vue
+++ vuejs/src/views/page/pages/Request.vue
@@ -8,7 +8,7 @@
         <div class="column is centered has-text-centered has-text-white is-two-fifths">
           <article :class=" errormessageVisibility ? 'message is-danger' : 'message is-hidden is-danger'">
             <div class="message-header">
-              <p>Error Logging in!!</p>
+              <p>Error Requesting !!</p>
               <button class="delete" @click="errormessageVisibility = false" aria-label="delete"></button>
             </div>
             <div class="message-body">
@@ -37,33 +37,37 @@
                 </span>
               </p>
             </div>
-              <div class="field">
-                <p class="control has-icons-left has-icons-right">
-                  <input class="input is-rounded is-focused" placeholder="Email" id="email" type="email" v-model="email" required>
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-envelope"></i>
-                  </span>
-                  <span class="icon is-small is-right">
-                    <i class="fas fa-check"></i>
-                  </span>
-                </p>
-              </div>
-              <div class="field">
-                <div class="control">
-                  <textarea class="textarea is-success is-rounded" placeholder="Why You Need Access ?" id="message" rows="3" v-model="message" required></textarea>
-                  <p class="help is-success">How did You Know about this and Why do You need?</p>
-                </div>
-              </div>
-              <label class="checkbox mb-3">
-                <input class="has-text-success" type="checkbox" id="terms" name="terms" v-model="checked">
-                  <span class="content has-text-white">  I Accept and Read the <a class="has-text-success" href="https://github.com/tks18/gindex-v4/blob/dark-mode-0-1/CODE_OF_CONDUCT.md" target="_blank">Community Guidelines</a></span>
-              </label>
-              <button :class=" loading ? 'button is-loading is-warning is-medium' : 'button is-warning is-medium' " type="submit" :disabled="disabled" >
-                <span class="icon">
-                  <i class="fas fa-user-plus"></i>
+            <div class="field">
+              <p class="control has-icons-left has-icons-right">
+                <input class="input is-rounded is-focused" placeholder="Email" id="email" type="email" v-model="email" required>
+                <span class="icon is-small is-left">
+                  <i class="fas fa-envelope"></i>
                 </span>
-                <span>Request Access</span>
-              </button>
+                <span class="icon is-small is-right">
+                  <i class="fas fa-check"></i>
+                </span>
+              </p>
+            </div>
+            <div class="field">
+              <div class="control">
+                <textarea class="textarea is-success is-rounded" placeholder="Why You Need Access ?" id="message" rows="3" v-model="message" required></textarea>
+                <p class="help is-success">How did You Know about this and Why do You need?</p>
+              </div>
+            </div>
+            <label class="checkbox mb-3">
+              <input class="has-text-success" type="checkbox" id="terms" name="terms" v-model="checked">
+                <span class="content has-text-white">  I Accept and Read the <a class="has-text-success" href="https://raw.githubusercontent.com/tks18/gindex-v4/dark-mode-0-1/CONTRIBUTING.md" target="_blank">Community Guidelines</a></span>
+            </label>
+            <label class="checkbox mb-3">
+              <input class="has-text-success" type="checkbox" id="code" name="terms" v-model="codechecked">
+                <span class="content has-text-white">  I Accept and Read the <a class="has-text-success" href="https://github.com/tks18/gindex-v4/blob/dark-mode-0-1/CODE_OF_CONDUCT.md" target="_blank">Code of Conduct</a></span>
+            </label>
+            <button :class=" loading ? 'button is-loading is-rounded is-warning is-medium' : 'button is-warning is-rounded is-medium' " type="submit" :disabled="disabled" >
+              <span class="icon">
+                <i class="fas fa-user-plus"></i>
+              </span>
+              <span>Request Access</span>
+            </button>
           </form>
         </div>
         <div class="column is-half">
@@ -112,6 +116,7 @@ import 'vue-loading-overlay/dist/vue-loading.css';
                 resultmessage: "",
                 databasemessage: "",
                 checked: "",
+                codechecked: "",
                 loading: false,
                 disabled: true,
                 fullpage: true,
@@ -125,30 +130,38 @@ import 'vue-loading-overlay/dist/vue-loading.css';
               this.loading = true;
                 e.preventDefault()
                 if(this.checked){
-                  let url = window.apiRoutes.requestRoute
-                  this.$http.post(url, {
-                        name: this.name,
-                        email: this.email,
-                        message: this.message,
-                  })
-                  .then(response => {
-                      if(response){
-                        if(response.data.auth && response.data.registered){
-                          this.successmessageVisibility = true;
-                          this.errormessageVisibility = false;
-                          this.loading = false;
-                          this.resultmessage = response.data.message
-                        } else {
-                          this.successmessageVisibility = false;
-                          this.errormessageVisibility = true;
-                          this.loading = false;
-                          this.resultmessage = response.data.message
+                  if(this.codechecked){
+                    let url = window.apiRoutes.requestRoute
+                    this.$http.post(url, {
+                          name: this.name,
+                          email: this.email,
+                          message: this.message,
+                    })
+                    .then(response => {
+                        if(response){
+                          if(response.data.auth && response.data.registered){
+                            this.successmessageVisibility = true;
+                            this.errormessageVisibility = false;
+                            this.loading = false;
+                            this.resultmessage = response.data.message
+                          } else {
+                            this.successmessageVisibility = false;
+                            this.errormessageVisibility = true;
+                            this.loading = false;
+                            this.resultmessage = response.data.message
+                          }
                         }
-                      }
-                  })
-                  .catch(error => {
-                      console.error(error);
-                  });
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+                  } else {
+                    this.successmessageVisibility = false;
+                    this.errormessageVisibility = true;
+                    this.loading = false;
+                    this.resultmessage = "> You Need to Accept Code of Conduct."
+                    this.checked = false;
+                  }
                 } else {
                   this.successmessageVisibility = false;
                   this.errormessageVisibility = true;
@@ -159,22 +172,10 @@ import 'vue-loading-overlay/dist/vue-loading.css';
             },
         },
         watch: {
-          checked: function() {
+          codechecked: function() {
             const emailRegex = /[a-z1-9]+@+[a-z1-9A-Z]+[.][a-z]+/g
-            if(emailRegex.test(this.email)){
-              if(this.name.length > 0){
-                if(this.message.length > 0){
-                  if(this.checked){
-                    this.disabled = false;
-                  } else {
-                    this.disabled = true;
-                  }
-                } else {
-                  this.disabled = true;
-                }
-              } else {
-                this.disabled = true;
-              }
+            if(emailRegex.test(this.email) && this.name.length > 0 && this.message.length > 0 && this.checked && this.codechecked){
+              this.disabled = false;
             } else {
               this.disabled = true;
             }
