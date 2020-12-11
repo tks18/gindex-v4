1c84a0b2f0769ed9dc8eeefb7716b2f3bfe73ed0
tks18 <tksudharshan@gmail.com>
2020-12-11 16:36:44 +0530

    CLI: Now Generate Worker Code in cli tool, Direct deployment is WIP

--- cli-tool/commands/deploy/frontend/getAllPrompts.js
+++ cli-tool/commands/deploy/frontend/getAllPrompts.js
@@ -1,5 +1,10 @@
 const inputPrompts = require('./inputPrompts');
 
+const open = require('open');
+const chalk = require('chalk');
+
+const authUrl = 'https://accounts.google.com/o/oauth2/auth?client_id=202264815644.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive&approval_prompt=auto';
+
 module.exports = async () => {
   let objs = {};
   objs.appname = await inputPrompts.appname();
@@ -11,6 +16,11 @@ module.exports = async () => {
   objs.favicon = await inputPrompts.favicon();
   objs.clientId = await inputPrompts.clientId();
   objs.clientSecret = await inputPrompts.clientSec();
+  let getAuthCode = await inputPrompts.getAuthCode();
+  if(getAuthCode){
+    console.log(chalk.yellow.bold("\nOpening Auth Code Generator in Your Browser\n"))
+    await open(authUrl);
+  }
   objs.refreshToken = await inputPrompts.refreshTok();
   objs.roots = [];
   let root0 = await inputPrompts.rootForm();
--- cli-tool/commands/deploy/frontend/index.js
+++ cli-tool/commands/deploy/frontend/index.js
@@ -3,8 +3,27 @@ const getLatIndex = require('./getLatIndex');
 const replaceOpt = require('./replaceOpt');
 const getAllPrompts = require('./getAllPrompts');
 const inputPrompts = require('./inputPrompts');
+const spinner = require('../../../helpers/spinner');
+
 const open = require('open');
+const chalk = require('chalk');
+const clipboardy = require('clipboardy');
 
 module.exports = async () => {
-
+  const getmode = await inputPrompts.mode();
+  if(getmode == "only-code"){
+    spinner(true, 'Getting the Latest Index Code', 0, false, async (indexSpin) => {
+      const latestIndex = await getLatIndex()
+      indexSpin.stop();
+      console.log(chalk.green.bold("\nIndex Code has been Successfully Retrived, Now Enter the Required Details\n"))
+      const getAllObjs = await getAllPrompts();
+      const finalIndexCode = await replaceOpt(latestIndex.code, getAllObjs, latestIndex.verNumber)
+      clipboardy.writeSync(finalIndexCode);
+      console.log(chalk.green.bold("\n Your Code has been Copied to Clipboard, You can Deploy by Pasting to Your Worker in Cloudflare\n"))
+      process.exit();
+    })
+  } else if(getmode == "direct-deploy"){
+    console.log(chalk.yellow.bold("\nCurrently Under Development, Check Some Days After\n"))
+    process.exit();
+  }
 }
