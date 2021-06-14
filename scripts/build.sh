ab3c6967f896d7054b3818a0ff6fdb0432871ffa
tks18 <tksudharshan@gmail.com>
2021-06-14 08:30:07 +0530

    Update Final Build Script

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,19 +11,20 @@ printf "\n"
 printf "Getting Latest themes and Variants"
 themes_list=("gradient-carnation" "gradient-curious-blue" "gradient-emerald" "gradient-ice-cold" "gradient-konifer" "gradient-netflix-red" "gradient-kournikova" "gradient-mona-lisa" "gradient-persian-rose" "gradient-purple-heart" "gradient-purple-mountains-majesty" "gradient-salmon" "gradient-selective-yellow" "gradient-shamrock" "gradient-witch-haze" "black-carnation" "black-curious-blue" "black-emerald" "black-ice-cold" "black-konifer" "black-netflix-red" "black-kournikova" "black-mona-lisa" "black-persian-rose" "black-purple-heart" "black-purple-mountains-majesty" "black-salmon" "black-selective-yellow" "black-shamrock" "black-witch-haze")
 
-printf "\nDeleting old Builds"
+printf "\nDeleting old Builds\n\n"
 ls
-rm -rfv outputs/.
+git rm -r outputs/.
 dest_dir="outputs/themes"
 mkdir -p dest_dir
 cd outputs
 ls
+cd ..
 
-# for theme in ${themes_list[@]}; do
-#     export VUE_APP_THEME=${theme}
-#     printf "\nBuilding Assets for ${theme}\n\n"
-#     theme_dir="${dest_dir}/${theme}"
-#     mkdir -p ${theme_dir}
-#     yarn build
-#     mv -v ./dist/* ${theme_dir}/.    
-# done
+for theme in ${themes_list[@]}; do
+    export VUE_APP_THEME=${theme}
+    printf "\nBuilding Assets for ${theme}\n\n"
+    theme_dir="${dest_dir}/${theme}"
+    mkdir -p ${theme_dir}
+    yarn build
+    mv -v ./dist/* ${theme_dir}/.    
+done
