b66ca7c5683aec9faad652dfd7defb75af0f1d13
tks18 <tksudharshan@gmail.com>
2021-06-14 10:38:45 +0000

    Preparing for Frontend Deploy: 80b61d0b35c9f31f95507c1b5d8d108961ca46f4-Merge pull request #269 from tks18/dev
    Dev

--- backend/plugins/checkOrigin.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (origin){
-  var allowedOrigins = process.env.FRONTENDURL.split(",");
-  if (origin && allowedOrigins.indexOf(origin) > -1){
-    return true
-  } else {
-    return false
-  }
-}
--- vuejs/src/plugin/video-plugins/index.js
+++
@@ -1,4 +0,0 @@
-import Flv from "./flv";
-import Hls from "./hls";
-
-export { Flv, Hls };
