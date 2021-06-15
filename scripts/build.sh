7d49690bf8ef0b36ce41521f6ed3c5d01fb62f84
tks18 <tksudharshan@gmail.com>
2021-06-15 18:05:10 +0530

    CI: Fix Env in Deploy Script CI: Use rm instead of Git rm

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,10 @@
 cd vuejs
 printf ".\n.\n.\n.\n.\n"
 printf "Setting Default Environment Variables"
-export VUE_APP_I18N_LOCALE=zh-chs
+export VUE_APP_I18N_LOCALE=en
 export VUE_APP_I18N_FALLBACK_LOCALE=en
 export VUE_APP_SECRET_PASS=${SECRET_PASS}
-export VUE_APP_CDN_PATH=https://cdn.jsdelivr.net/gh/tks18/gindex-v4@master/vuejs/outputs/THEME/
+export VUE_APP_CDN_PATH=https://cdn.jsdelivr.net/gh/tks18/gindex-v4@master/themes/THEME/
 printf "\n"
 printf "Getting Latest themes and Variants"
 themes_list=("gradient-carnation" "gradient-curious-blue" "gradient-emerald" "gradient-ice-cold" "gradient-konifer" "gradient-netflix-red" "gradient-kournikova" "gradient-mona-lisa" "gradient-persian-rose" "gradient-purple-heart" "gradient-purple-mountains-majesty" "gradient-salmon" "gradient-selective-yellow" "gradient-shamrock" "gradient-witch-haze" "black-carnation" "black-curious-blue" "black-emerald" "black-ice-cold" "black-konifer" "black-netflix-red" "black-kournikova" "black-mona-lisa" "black-persian-rose" "black-purple-heart" "black-purple-mountains-majesty" "black-salmon" "black-selective-yellow" "black-shamrock" "black-witch-haze")
