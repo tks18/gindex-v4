c2cfd1191924fbaadc314f45568fe87bbacae5f6
tks18 <tksudharshan@gmail.com>
2021-06-25 08:39:47 +0530

    Update Build & Deploy Scripts for Development Builds

---
+++ scripts/dev-after-deploy.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+printf "\nDoing After Deployment Activities\n"
+
+GIT_URL="github.com/tks18/gindex-v4.git"
+FT_DEPLOY_DIR="ft-deploy-tmp"
+BE_DEPLOY_DIR="be-deploy-tmp"
+CLI_DEPLOY_DIR="cli-deploy-tmp"
+
+git config --global user.name "tks18"
+git config --global user.email "tksudharshan@gmail.com"
+
+printf "\nPublishing Frontend\n"
+rm -rf vuejs/node_modules/*
+rm -rf vuejs/.yarn/cache/*
+rm -rf vuejs/outputs/*
+git clone --single-branch --depth 3 --quiet -b frontend-dev https://tks18:${GITHUB_TOKEN}@${GIT_URL} ${FT_DEPLOY_DIR}
+cd ${FT_DEPLOY_DIR}
+rm -rf *
+cp -r ../vuejs/. .
+git add .
+git commit -m "Deploying Frontend for ${TRAVIS_COMMIT}-${TRAVIS_COMMIT_MESSAGE}"
+git push --quiet -u --no-progress origin frontend-dev
+
+cd ..
+rm -rf ${FT_DEPLOY_DIR}/*
+
+printf "\nPublishing Backend\n"
+git clone --single-branch --depth 3 --quiet -b backend-dev https://tks18:${GITHUB_TOKEN}@${GIT_URL} ${BE_DEPLOY_DIR}
+cd ${BE_DEPLOY_DIR}
+rm -rf *
+cp -r ../backend/. .
+git add .
+git commit -m "Deploying Backend for ${TRAVIS_COMMIT}-${TRAVIS_COMMIT_MESSAGE}"
+git push --quiet -u --no-progress origin backend-dev
+
+cd ..
+rm -rf ${BE_DEPLOY_DIR}/*
+
+printf "\nPublishing CLI Tool\n"
+git clone --single-branch --depth 3 --quiet -b cli-tool-dev https://tks18:${GITHUB_TOKEN}@${GIT_URL} ${CLI_DEPLOY_DIR}
+cd ${CLI_DEPLOY_DIR}
+rm -rf *
+cp -r ../cli-tool/. .
+git add .
+git commit -m "Deploying CLI-Tool for ${TRAVIS_COMMIT}-${TRAVIS_COMMIT_MESSAGE}"
+git push --quiet -u --no-progress origin cli-tool-dev
+
+cd ..
+
+rm -rf ${CLI_DEPLOY_DIR}/*
+rm -rf vuejs/*
+rm -rf backend/*
+rm -rf cli-tool/* 
+
+printf "Successfully Deployed all the Modules"
+
---
+++ scripts/dev-deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+GIT_URL="github.com/tks18/gindex-v4.git"
+DEPLOY_DIR="deploy-tmp"
+
+git config --global user.name "tks18"
+git config --global user.email "tksudharshan@gmail.com"
+
+git clone --single-branch --depth 3 --quiet -b dev-build https://tks18:${GITHUB_TOKEN}@${GIT_URL} ${DEPLOY_DIR}
+cd ${DEPLOY_DIR}
+rm -rf *
+cp -r ../vuejs/outputs/. .
+mkdir worker
+cp ../worker/. worker/.
+git add .
+git commit -m "Deploying ${TRAVIS_BUILD_ID}-${TRAVIS_BUILD_NUMBER}: ${TRAVIS_COMMIT}-${TRAVIS_COMMIT_MESSAGE}"
+git push origin dev-build
