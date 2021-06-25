b1460b901d42e7a3a34d338c73c0727765c5f8cd
tks18 <tksudharshan@gmail.com>
2021-06-25 08:54:51 +0530

    Scripts: Auto Deployment of Version manager tool

--- scripts/prod-after-deploy.sh
+++ scripts/prod-after-deploy.sh
@@ -6,6 +6,7 @@ GIT_URL="github.com/tks18/gindex-v4.git"
 FT_DEPLOY_DIR="ft-deploy-tmp"
 BE_DEPLOY_DIR="be-deploy-tmp"
 CLI_DEPLOY_DIR="cli-deploy-tmp"
+VM_DEPLOY_DIR="vm-deploy-tmp"
 
 git config --global user.name "tks18"
 git config --global user.email "tksudharshan@gmail.com"
@@ -47,8 +48,20 @@ git commit -m "Deploying CLI-Tool for ${TRAVIS_COMMIT}-${TRAVIS_COMMIT_MESSAGE}"
 git push --quiet -u --no-progress origin cli-tool
 
 cd ..
-
 rm -rf ${CLI_DEPLOY_DIR}/*
+
+printf "\nPublishing Version Manager Tool\n"
+git clone --single-branch --depth 3 --quiet -b version-manager-dev https://tks18:${GITHUB_TOKEN}@${GIT_URL} ${VM_DEPLOY_DIR}
+cd ${VM_DEPLOY_DIR}
+rm -rf *
+cp -r ../cli-tool/. .
+git add .
+git commit -m "Deploying Version Manager for ${TRAVIS_COMMIT}-${TRAVIS_COMMIT_MESSAGE}"
+git push --quiet -u --no-progress origin version-manager-dev
+
+cd ..
+rm -rf ${VM_DEPLOY_DIR}/*
+
 rm -rf vuejs/*
 rm -rf backend/*
 rm -rf cli-tool/* 
