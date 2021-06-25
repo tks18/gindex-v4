cc94ebb4a6886d71f5ca6784404fe26c71067e4a
tks18 <tksudharshan@gmail.com>
2021-06-25 10:03:39 +0530

    Update Script

--- scripts/before-build.sh
+++ scripts/before-build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]]
-then
+if [ ${TRAVIS_BRANCH} == "master" ] && [ ${TRAVIS_PULL_REQUEST} == "false" ]; then
     bash ./scripts/prod-build.sh
 else
     bash ./scripts/dev-build.sh
