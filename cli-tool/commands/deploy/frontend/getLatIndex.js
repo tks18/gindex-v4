0a85ecee7ae4116bb57466b6b3b861c6c2cfa474
tks18 <tksudharshan@gmail.com>
2021-10-13 19:12:26 +0530

    CLI Tool: Fix Unresponsive Screen of Cli tool; Fix Frontend/backend deployment

--- cli-tool/commands/deploy/frontend/getLatIndex.js
+++ cli-tool/commands/deploy/frontend/getLatIndex.js
@@ -2,17 +2,23 @@ const axios = require('axios');
 const frontendRegex = /^(frontend).+/;
 
 module.exports = async () => {
-  return axios.get('https://api.github.com/repos/tks18/gindex-v4/releases').then(resp => {
-    const result = resp.data.filter(releases =>{
-      return frontendRegex.test(releases.tag_name);
-    })[0].tag_name
-    return axios.get(`https://raw.githubusercontent.com/tks18/gindex-v4/${result}/worker/index.js`).then(resp => {
-      replacedVersion = result.replace(/^(frontend-)/, '')
-      return {
-        version: result,
-        verNumber: replacedVersion,
-        code: resp.data
-      }
-    })
-  })
-}
+  return axios
+    .get('https://api.github.com/repos/tks18/gindex-v4/releases')
+    .then((resp) => {
+      const result = resp.data.filter((releases) => {
+        return frontendRegex.test(releases.tag_name) && !releases.prerelease;
+      })[0].tag_name;
+      return axios
+        .get(
+          `https://raw.githubusercontent.com/tks18/gindex-v4/master/worker/index.js`,
+        )
+        .then((resp) => {
+          replacedVersion = result.replace(/^(frontend-)/, '');
+          return {
+            version: result,
+            verNumber: replacedVersion,
+            code: resp.data,
+          };
+        });
+    });
+};
